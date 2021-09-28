0d536a4989831ea10f4135ec4fffb705f52ab6bc
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-09-28 16:53:59 +0300

    deploy script: use gas strategy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -9,6 +9,8 @@ def main():
     print(network.show_active())
     publish_source = True if os.getenv("ETHERSCAN_TOKEN") else False
     decimals = int(os.getenv("TOKEN_DECIMALS"))
+    from brownie.network.gas.strategies import GasNowScalingStrategy
+    gas_strategy = GasNowScalingStrategy("standard", "fast", 1.2)
     deploy_args = [
         os.getenv("TOKEN_ADDRESS"),
         os.getenv("VALIDATOR_ADDRESS"),
@@ -16,7 +18,7 @@ def main():
         [strtobool(limit) for limit in os.getenv("SWAP_ENABLED").split(',')],
         int(os.getenv("MIN_SWAP_AMOUNT_PER_TX")) * (10 ** decimals),
         int(os.getenv("MAX_SWAP_AMOUNT_PER_TX")) * (10 ** decimals),
-        {"from": dev},
+        {"from": dev, 'gas_price': gas_strategy},
     ]
     print('Deploy args:\n', deploy_args)
     if input("Deploy SwapContract? y/[N]: ").lower() != "y":
